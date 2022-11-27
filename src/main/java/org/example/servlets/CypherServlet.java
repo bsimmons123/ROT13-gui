ed1d6b2a24c4ca8fce82e1419dc2feed1926c362
Brandon Simmons <bsimmons1@ycp.edu>
2022-11-27 16:17:06 -0500

    first commit

---
+++ src/main/java/org/example/servlets/CypherServlet.java
@@ -0,0 +1,20 @@
+package org.example.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/add")
+public class AddServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("GET method from AddServlet");
+    }
+}
