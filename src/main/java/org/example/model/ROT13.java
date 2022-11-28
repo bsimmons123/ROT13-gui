cf3009975f2267786267043b646976fb40d5cdae
Brandon Simmons <bsimmons1@ycp.edu>
2022-11-27 20:53:09 -0500

    adding all files

--- src/main/java/org/example/model/ROT13.java
+++ src/main/java/org/example/model/ROT13.java
@@ -1,2 +1,11 @@
-package org.example.model;public enum ROT13 {
+package org.example.model;
+
+public enum ROT13 {
+    CYPHER("abcdefghijklmnopqrstuvwxyz");
+
+    public final String hash;
+    private ROT13(String hash) {
+        this.hash = hash;
+    }
+
 }
--- src/main/java/org/example/servlets/CypherServlet.java
+++ src/main/java/org/example/servlets/CypherServlet.java
@@ -1,5 +1,9 @@
 package org.example.servlets;
 
+import org.example.model.ROT13;
+import org.omg.CORBA.INTERNAL;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +11,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-@WebServlet(urlPatterns = "/add")
-public class AddServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/cypher.start")
+public class CypherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.println("GET method from AddServlet");
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String values = req.getParameter("input-value"); // getting values from JSP page
+
+        req.setAttribute("hashedValues", hashIndexedValues(hashValues(values)));
+        doGet(req, resp);
+    }
+
+    private ArrayList<Integer> hashValues(String values){
+        ArrayList<Integer> indexValues = new ArrayList<>();
+        for (char hashVal:  values.toCharArray()) {
+            int hashedIndexValue = ROT13.CYPHER.hash.indexOf(hashVal);
+            if (hashedIndexValue >= 0){
+                indexValues.add(hashedIndexValue);
+            }
+        }
+        ArrayList<Integer> hashedIndexValues = new ArrayList<>();
+        for(int i = 0; i < indexValues.size(); i++){
+            hashedIndexValues.add((indexValues.get(i)+13)%26);
+        }
+
+        return hashedIndexValues;
+    }
+
+    private String hashIndexedValues(ArrayList<Integer> hashedIndexValues){
+        String hashedValues = "";
+        for (int value: hashedIndexValues) {
+            hashedValues += ROT13.CYPHER.hash.subSequence(value, value+1);
+        }
+        return hashedValues;
     }
 }
